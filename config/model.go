2e34b94a738197b8b3cae3fe0790000d5f2af6ee
milossimic <milossimicsimo@gmail.com>
2018-11-23 14:17:40 +0100

    started work on storage and plumbing

---
+++ config/model.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type Celestial struct {
+	Conf Config `yaml:"gravity"`
+}
+
+type Config struct {
+	ConfVersion string   `yaml:"version"`
+	Address     string   `yaml:"address"`
+	Endpoints   []string `yaml:"db"`
+	SEndpoints  []string `yaml:"sdb"`
+}
+
+func ConfigFile(n ...string) (*Config, error) {
+	path := "config.yml"
+	if len(n) > 0 {
+		path = n[0]
+	}
+
+	yamlFile, err := ioutil.ReadFile(path)
+	check(err)
+
+	var conf Celestial
+	err = yaml.Unmarshal(yamlFile, &conf)
+	check(err)
+
+	return &conf.Conf, nil
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
